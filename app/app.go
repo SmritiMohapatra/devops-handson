5f9ef41a0429a3bd70b158294380d562dd46afde
Smriti Mohapatra <SmritiMohapatra@users.noreply.github.com>
2019-05-27 13:06:25 +0530

    Update app.go

--- app/app.go
+++ app/app.go
@@ -6,7 +6,7 @@ import (
 )
 
 func helloWorld(w http.ResponseWriter, r *http.Request){
-    fmt.Fprintf(w, "Hello DevOps")
+    mt.Fprintf(w, "Hello DevOps")
     fmt.Fprintf(w, "Step 1")
     fmt.Fprintf(w, "Step oh")
 }
